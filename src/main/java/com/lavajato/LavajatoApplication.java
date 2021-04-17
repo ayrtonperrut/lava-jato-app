6db25a889c27bb5fc87197332210b10c49930b23
ayrton <ayrton.perrut@projuris.com.br>
2021-04-17 15:13:27 -0300

    TRF-002
    retirada da mensagem de teste

--- src/main/java/com/lavajato/LavajatoApplication.java
+++ src/main/java/com/lavajato/LavajatoApplication.java
@@ -12,7 +12,6 @@ public class LavajatoApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(LavajatoApplication.class, args);
-		System.out.println("Estou funcionando!");
 	}
 
 }
